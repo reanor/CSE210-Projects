efe3df8e1c83c88b2a3902fb07bbd600a12396c1
sva16001@byui.edu <sva16001@byui.edu>
2025-09-19 17:19:37 -0400

    C# Prep 1 and Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,29 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter;
+
+        if (number >= 90)
+        {
+            letter = "A - You Passed!";
+        }
+        else if (number >= 80)
+        {
+            letter = "B - You Passed!";
+        }
+        else if (number >= 70)
+        {
+            letter = "C - You Passed!";
+        }
+        else
+        {
+            letter = "lower than required to pass this class. Maybe next time!";
+        }
+        Console.WriteLine($"Your grade is - " + letter);
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Security.Permissions;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This is in C#!");
+
+        // int number = 5; 
+        // number = 8;
+        // number = number + 3;
+
+        // string color = "blue";
+
+        // if (number > 3)
+        // {
+        //     Console.WriteLine("");
+        // }
+
+        // Console.ReadLine(); - //storing
+        Console.Write("Whats your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is {color}");
+
     }
 }
